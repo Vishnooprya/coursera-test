7c7989862166063962fd585f13cd68ad44163e4f
Vishnooprya <vishnoopriya11@gmail.com>
2020-05-04 14:48:53 +0530

    New Folder

---
+++ module4-solution/data/categories.component.js
@@ -0,0 +1,11 @@
+(function(){
+  'use strict';
+  angular.module('data')
+  .component('categories',{
+    templateUrl:'data/categories.html',
+    bindings:{
+      input:'<'
+    }
+
+  });
+})();
